625f56ef2e8202310f3897ba16597c632cb9816a
sippako <min001aqw@gmail.com>
2021-10-13 15:43:14 +0700

    update

---
+++ js/while.js
@@ -0,0 +1,13 @@
+var myNumber = ['a','b','c','d','e'];
+var idx = 0;
+var number = myNumber.length;//5
+console.log("Length = " + number);
+
+console.log("Member is " + myNumber[0]);
+console.log("Member is " + myNumber[1]);
+
+while(idx < number){
+    myNumber[idx];
+    idx = idx + 1;
+}   
+console.log(myNumber);
